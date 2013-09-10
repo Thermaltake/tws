920ff84bc80e64ba65a81e515b2572dd18fbbfac
unknown <slavikwow2009@yandex.ru>
2013-09-11 01:32:12 +0400

    Add function ScreeningText ($str)

--- core.php
+++ core.php
@@ -4,3 +4,21 @@
 
 class Cookie {
    var $lang = 0; }
+
+
+function ScreeningText ($text)
+{
+   $text = str_replace( "&"				, "&amp;"         , $text );
+   $text = str_replace( ">"				, "&gt;"          , $text );
+   $text = str_replace( "<"				, "&lt;"          , $text );
+   $text = str_replace( "\\\\"	 	, "&#092;"        , $text );
+   $text = str_replace( "\'"			, "&#39;"         , $text );
+   $text = str_replace( "\""			, "&quot;"        , $text );
+   $text = str_replace( '"'				, "&quot;"        , $text );
+   $text = str_replace( "$"				, "&#036;"        , $text );
+   $text = str_replace( "!"				, "&#33;"         , $text );
+   $text = str_replace( "'"				, "&#39;"         , $text );
+   $text = str_replace( "\&#39;"  , "&#39;"         , $text );
+   $text = str_replace( "\&quot;" , "&quot;"        , $text );
+   return $text;
+}
