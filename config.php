84aff9900c4706f563ba11d5789eaecdd84aa2b1
unknown <slavikwow2009@yandex.ru>
2013-09-11 19:28:19 +0400

    Add config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?
+
+/* Encoding / Кодировка */
+
+$default_language = '8'; // Default russian id 8
+$fail_log = true; // Log fail
+$use_captcha = true; // Use captcha for reg and login
+$check_reg_double_email = false; // Check in registration duble email
+$check_ban_ip_reg = false; // Check ban ip in reg
+$time_zone = ''; // Не скоро будет....
+$use_caching_MySQL = true; // Caching data from MySQL
