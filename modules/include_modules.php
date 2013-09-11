bf0ef53b98a86504b2dc0fff0e3d1287ce3ebf4b
unknown <slavikwow2009@yandex.ru>
2013-09-11 19:04:18 +0400

    Add 404 use module and correct switch

--- modules/include_modules.php
+++ modules/include_modules.php
@@ -4,15 +4,20 @@
 
 /* Это пока будет для модулей, которые как то подписанны и добавлены */
 
-switch ($p[1])
+if ($p[1])
 {
-    case "home": include ("modules/m_home/$mo"); break; // Home page
-    case "stat": include ("modules/m_stat/$mo"); break; // stat page
-    case "reg": include ("modules/m_reg/$mo"); break; // registration page
-    case "ch_info": include ("modules/m_ch_info/$mo"); break; // Characters info page
-    case "t_info": include ("modules/m_t_info/$mo"); break; // Arena team info page
-    case "g_info": include ("modules/m_g_info/$mo"); break; // guild info page
-    default "home": include ("modules/m_home/home.php"); // Home page
+    switch ($p[1])
+    {
+        case "home": include ("modules/m_home/$mo"); break; // Home page
+        case "stat": include ("modules/m_stat/$mo"); break; // stat page
+        case "reg": include ("modules/m_reg/$mo"); break; // registration page
+        case "ch_info": include ("modules/m_ch_info/$mo"); break; // Characters info page
+        case "t_info": include ("modules/m_t_info/$mo"); break; // Arena team info page
+        case "g_info": include ("modules/m_g_info/$mo"); break; // guild info page
+        default: include ("modules/m_404/404.php"); // Home page
+    }
 }
+else
+    include ("modules/m_home/home.php");
 
 /* Это будет для модулей, которые не были ни где добавлены. Будет идти поиск папки с модулем. */
