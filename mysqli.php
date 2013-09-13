9a91407f899a8dbdf48038fde02831863b6d9b90
unknown <slavikwow2009@yandex.ru>
2013-09-13 21:40:06 +0400

    Connections realms db

--- mysqli.php
+++ mysqli.php
@@ -8,4 +8,11 @@ class sql extends mysqli {
         parent::real_connect($host, $user, $pass, $db);
     }
 }
+
+
+while ($connections[$i->realms])
+{
+   $con[$i->realms] = new sql($connections[$i->realms]['ip'], $connections[$i->realms]['user'], $connections[$i->realms]['pass'], $connections[$i->realms]['db']);
+   $i->realms++;
+}
 /* С функциямии mysqli еще не работал в php.. Будем учиться ;) */
