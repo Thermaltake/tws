cc6e62a9a696f8a74abda0dbef08f9d95086ce35
unknown <slavikwow2009@yandex.ru>
2013-09-13 21:28:17 +0400

    Add settings connection to db

--- config.php
+++ config.php
@@ -11,3 +11,43 @@ $time_zone = ''; // Не скоро будет....
 $use_caching_MySQL = true; // Caching data from MySQL
 $cache_time[0] = '60'; // in sec, 0 - data character stat
 $cache_time[1] = '1800'; // in sec, 1 - data world stat
+
+$connections = array (
+
+    "accounts" => array ( // IF id == accounts { realmd }
+        "ip" => "localhost", // IP
+        "db" => "realmd", // Database name realmd
+        ),
+
+    "site" => array ( // IF id == site { site }
+        "ip" => "localhost", // IP
+        "db" => "site", // Database name site
+        ),
+
+    "world" => array ( // IF id == site { site }
+        "ip" => "localhost", // IP
+        "db" => "mangos", // Database name world
+        ),
+
+    /* Configuration realms */
+    "0" => array ( // id realm
+        "ip" => "localhost", // IP
+        "db" => "characters_1", // Database realmd
+        "gp" => "8085", // Game Port
+        "rn" => "WoW x1" // Realm name
+        ),
+
+    "1" => array ( // id realm
+        "ip" => "localhost", // IP
+        "db" => "characters_2", // Database realmd
+        "gp" => "8086", // Game Port
+        "rn" => "WoW x2" // Realm name
+        ),
+
+    "2" => array ( // id realm
+        "ip" => "localhost", // IP
+        "db" => "characters_3", // Database realmd
+        "gp" => "8087", // Game Port
+        "rn" => "WoW x3" // Realm name
+        ),
+);
