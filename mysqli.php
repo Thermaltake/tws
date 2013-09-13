bd11c09842823fed27452fca442cba86d9085ce6
unknown <slavikwow2009@yandex.ru>
2013-09-13 19:54:44 +0400

    add class sql use mysqli

--- mysqli.php
+++ mysqli.php
@@ -2,4 +2,10 @@
 
 /* кодировка */
 
+class sql extends mysqli {
+    public function __construct($host, $user, $pass, $db) {
+        parent::init();
+        parent::real_connect($host, $user, $pass, $db);
+    }
+}
 /* С функциямии mysqli еще не работал в php.. Будем учиться ;) */
