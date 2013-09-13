9d3e86886e050988cdc6e14bdcf8f0c98bd0ab4c
unknown <slavikwow2009@yandex.ru>
2013-09-13 21:16:31 +0400

    Add cache time in clean_cache from config

--- cache_clean.php
+++ cache_clean.php
@@ -10,7 +10,7 @@ while ($dir[$i->dir])
     if (is_dir($dir[$i->dir]))
         if ($dh = opendir($dir[$i->dir])) {
            while (($file = readdir($dh)) !== false)
-               if (filectime($dir[$i->dir].$file) < time()-60 && filetype($dir[$i->dir] . $file) == 'file')
+               if (filectime($dir[$i->dir].$file) < time()-$cache_time[$i->dir] && filetype($dir[$i->dir] . $file) == 'file')
                    unlink ($dir[$i->dir].$file);
 
            closedir($dh); }
