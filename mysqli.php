54d0a6f182bfd6405ccf1b01c347aca205f10eb7
unknown <slavikwow2009@yandex.ru>
2013-09-14 10:56:40 +0400

    Update connections db

--- mysqli.php
+++ mysqli.php
@@ -9,13 +9,8 @@ class sql extends mysqli {
     }
 }
 
-$con['accounts'] = new sql($connections['accounts']['ip'], $connections['accounts']['user'], $connections['accounts']['pass'], $connections['accounts']['db']);
-$con['site'] = new sql($connections['site']['ip'], $connections['site']['user'], $connections['site']['pass'], $connections['site']['db']);
-$con['world'] = new sql($connections['world']['ip'], $connections['world']['user'], $connections['world']['pass'], $connections['world']['db']);
 
-while ($connections[$i->realms])
-{
-   $con[$i->realms] = new sql($connections[$i->realms]['ip'], $connections[$i->realms]['user'], $connections[$i->realms]['pass'], $connections[$i->realms]['db']);
-   $i->realms++;
-}
+foreach ($connections as $key => $value)
+    $con[$key] = new sql($connections[$key]['ip'], $connections[$key]['user'], $connections[$key]['pass'], $connections[$key]['db']);
+
 /* С функциямии mysqli еще не работал в php.. Будем учиться ;) */
