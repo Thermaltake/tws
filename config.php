3fcce07955039a76421a574bd369bbe623b15094
unknown <slavikwow2009@yandex.ru>
2013-09-18 13:31:22 +0400

    Del not need connection for site

--- config.php
+++ config.php
@@ -18,7 +18,6 @@ $cache->time[0] = '60'; // in sec, 0 - data character stat
 $cache->time[1] = '1800'; // in sec, 1 - data world stat
 
 $connections = array (
-
     "accounts" => array ( // IF id == accounts { realmd }
         "ip"   => "127.0.0.1", // IP
         "user" => "root", // Database username
@@ -39,32 +38,4 @@ $connections = array (
         "pass" => "", // Database password
         "db"   => "mangos", // Database name world
         ),
-
-    /* Configuration realms */
-    "0" => array ( // id realm
-        "ip"   => "127.0.0.1", // IP
-        "user" => "root", // Database username
-        "pass" => "", // Database password
-        "db"   => "characters_1", // Database realmd
-        "gp"   => "8085", // Game Port
-        "rn"   => "WoW x1" // Realm name
-        ),
-
-    /*"1" => array ( // id realm
-        "ip"   => "127.0.0.1", // IP
-        "user" => "root", // Database username
-        "pass" => "", // Database password
-        "db"   => "characters_2", // Database realmd
-        "gp"   => "8086", // Game Port
-        "rn"   => "WoW x2" // Realm name
-        ),
-
-    "2" => array ( // id realm
-        "ip"   => "127.0.0.1", // IP
-        "user" => "root", // Database username
-        "pass" => "", // Database password
-        "db"   => "characters_3", // Database realmd
-        "gp"   => "8087", // Game Port
-        "rn"   => "WoW x3" // Realm name
-        ),*/
 );
