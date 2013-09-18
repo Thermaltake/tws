d1563d68cc4aa7120e7884d871aedff65ce82b96
unknown <slavikwow2009@yandex.ru>
2013-09-18 15:52:25 +0400

    Add read cookie

--- read_cookie.php
+++ read_cookie.php
@@ -2,5 +2,7 @@
 
 /* кодировка */
 
-$cookie->lang = ((int)$_COOKIE['lang'] ? (int)$_COOKIE['lang'] : 8); // Set default lang id = 8 - Russian
-$cookie->hash = ScreeningText ($_COOKIE['hash']);
+$cookie->lang  = ((int)$_COOKIE['lang'] ? (int)$_COOKIE['lang'] : 8); // Set default lang id = 8 - Russian
+$cookie->hash  = ScreeningText ($_COOKIE['hash']);
+$cookie->guest = ScreeningText ($_COOKIE['guest']);
+$cookie->user  = ScreeningText ($_COOKIE['user']);
