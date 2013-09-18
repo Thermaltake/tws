9a44b9474a9c1b81440fba22c48746a047b2fdab
unknown <slavikwow2009@yandex.ru>
2013-09-18 16:31:19 +0400

    ...

--- params.php
+++ params.php
@@ -7,10 +7,12 @@ $uc = false; // Не менять!
 define ("DIR_ADDITIONAL_DATA", "additional_data/");
 define ("DIR_COMPONENTS",      "components/");
 define ("DIR_MODULES",         "modules/");
+define ("DIR_THEME",           "themes/");
 
 define ("FILE_CO", "co.php");
 define ("FILE_CD", "cd.php");
 
+define ("THEME_NAME", "wow");
 
 $uri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
 while ($uri[$i->url])
